cc2354578ef78b6d16f3db4dad73da0c1655ba2c
PrathamShid07 <pratham.shid23@spit.ac.in>
2025-07-19 09:32:25 +0530

    Delete backend/generate-vapid-keys.js

--- backend/generate-vapid-keys.js
+++
@@ -1,6 +0,0 @@
-// generate-vapid-keys.js
-const webpush = require('web-push');
-
-const vapidKeys = webpush.generateVAPIDKeys();
-console.log('VAPID_PUBLIC_KEY =', vapidKeys.publicKey);
-console.log('VAPID_PRIVATE_KEY =', vapidKeys.privateKey);
